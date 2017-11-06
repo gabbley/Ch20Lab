20482a6b9a4e6270347a9f3c54f1d9f0cfaec6fd
gabbley <baniquedg@bishopireton.org>
2017-11-06 13:20:03 -0500

    Im Not Really Sure

--- Lab II/src/Brokerage.java
+++ Lab II/src/Brokerage.java
@@ -1,17 +1,24 @@
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-public class Brokerage implements Login{
+public class Brokerage implements Login {
 
 	private TreeMap<String, Trader> registeredTraders;
 	private TreeSet<Trader> loggedInTraders;
-	
-	Brokerage(){
+	private StockExchange exchange;
+
+	Brokerage() {
 		registeredTraders = new TreeMap<String, Trader>();
 		loggedInTraders = new TreeSet<Trader>();
+		exchange = new StockExchange();
 	}
-	
-	
+
+	Brokerage(StockExchange ex) {
+		registeredTraders = new TreeMap<String, Trader>();
+		loggedInTraders = new TreeSet<Trader>();
+		exchange = ex;
+	}
+
 	@Override
 	public int addUser(String name, String password) {
 		int len = name.length();
@@ -22,38 +29,42 @@ public class Brokerage implements Login{
 			return -2;
 		if (registeredTraders.containsKey(name))
 			return -3;
-					
+
 		registeredTraders.put(name, new Trader(this, password, name));
 		return 0;
 	}
-	
+
 	public void getQuote(String symbol, Trader trader) {
-		
+		trader.receiveMessage(exchange.getQuote(symbol));
 	}
-	
+
 	@Override
 	public int login(String name, String password) {
 		Trader trader = registeredTraders.get(name);
-		
+
 		if (trader == null)
 			return -1;
 		String storedPassword = trader.getPassword();
 		if (!storedPassword.equals(password))
-			return -2; //screen name is not found
+			return -2; // screen name is not found
 		if (registeredTraders.containsValue(trader))
 			return -3;
-			
+
 		loggedInTraders.add(trader);
+		trader.openWindow();
+		if (!trader.hasMessages()){
+			trader.receiveMessage("Welcome to Safe Trade");
+		}
 		return 0;
 	}
-	
+
 	public void logout(Trader trader) {
 		if (loggedInTraders.contains(trader))
-		loggedInTraders.remove(trader);
+			loggedInTraders.remove(trader);
 	}
-	
+
 	public void placeOrder(TradeOrder order) {
 		
 	}
-	
+
 }
